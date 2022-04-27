ff7ef8440402df5ae2d028edcde836077df3834e
RikushiTakai <k021g1158@g.neec.ac.jp>
2022-04-27 17:43:54 +0900

    20220427の資料分

--- main.cpp
+++ main.cpp
@@ -187,6 +187,54 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	pipelineDesc.BlendState.RenderTarget[0].RenderTargetWriteMask =
 		D3D12_COLOR_WRITE_ENABLE_ALL;	//RGBA全てのチャンネルを描画
 
+	D3D12_RENDER_TARGET_BLEND_DESC& blenddesc = pipelineDesc.BlendState.RenderTarget[0];
+	blenddesc.RenderTargetWriteMask = D3D12_COLOR_WRITE_ENABLE_ALL; //RGBAすべてのチャンネルを描画
+
+	blenddesc.BlendEnable = true;					//ブレンドを有効にする
+
+	//あるふぁ～～～～
+	blenddesc.BlendOpAlpha = D3D12_BLEND_OP_ADD;	//加算
+	blenddesc.SrcBlendAlpha = D3D12_BLEND_ZERO;		//ソースの値を100%使う
+	blenddesc.DestBlendAlpha = D3D12_BLEND_ZERO;	//デストの値を  0%使う
+
+	enum BLEND_MODE
+	{
+		ADD,
+		SUB,
+		INV,
+		ALPHA,
+	};
+	int blendMode = ALPHA;
+
+	if (blendMode == ADD)
+	{
+		//加算合成
+		blenddesc.BlendOp = D3D12_BLEND_OP_ADD;			//加算
+		blenddesc.SrcBlend = D3D12_BLEND_ONE;			//ソースの値を100%使う
+		blenddesc.DestBlend = D3D12_BLEND_ONE;			//デストの値を100%使う
+	}
+	if (blendMode == SUB)
+	{
+		//減算合成
+		blenddesc.BlendOp = D3D12_BLEND_OP_REV_SUBTRACT;	//デストからソースを減算
+		blenddesc.SrcBlend = D3D12_BLEND_ONE;			//ソースの値を100%使う
+		blenddesc.DestBlend = D3D12_BLEND_ONE;			//デストの値を100%使う
+	}
+	if (blendMode == INV)
+	{
+		//色反転
+		blenddesc.BlendOp = D3D12_BLEND_OP_ADD;			//加算
+		blenddesc.SrcBlend = D3D12_BLEND_INV_DEST_COLOR;//1.0f-デストカラーの値
+		blenddesc.DestBlend = D3D12_BLEND_ZERO;			//使わない
+	}
+	if (blendMode == ALPHA)
+	{
+		//半透明合成
+		blenddesc.BlendOp = D3D12_BLEND_OP_ADD;			//加算
+		blenddesc.SrcBlend = D3D12_BLEND_SRC_ALPHA;		//ソースのアルファ値
+		blenddesc.DestBlend = D3D12_BLEND_INV_SRC_ALPHA;//1.0f-ソースのアルファ値
+	}
+
 	//頂点レイアウトの設定
 	pipelineDesc.InputLayout.pInputElementDescs = inputLayout;
 	pipelineDesc.InputLayout.NumElements = _countof(inputLayout);
